fae6996fc5d7dc5512f6b2e3034e29c5d0ff8719
Lola <lola.crowley.2019@mumail.ie>
2023-01-23 12:58:28 +0000

    Adding different sounds

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -3,56 +3,208 @@ let guideKeys = []; // All keys user needs to press to finish a guide
 let keysToPress = []; // Track keys a user still needs to press to finish a guide
 let guideStarted = false;
 let styleElem = document.head.appendChild(document.createElement("style"));
+let kbsound = 'Piano';
 
 
 // Add event listener on keydown
 document.addEventListener('keydown', (event) => {
     let name = event.key;
     // When key pressed call keyPressed method and pass in sound to play and name of key
-    switch (name) {
-        case "q": keyPressed("../Sounds/C3.mp3", "q"); break;
-        case "w": keyPressed("../Sounds/D3.mp3", "w"); break;
-        case "e": keyPressed("../Sounds/E3.mp3", "e"); break;
-        case "r": keyPressed("../Sounds/F3.mp3", "r"); break;
-        case "t": keyPressed("../Sounds/G3.mp3", "t"); break;
-        case "y": keyPressed("../Sounds/A3.mp3", "y"); break;
-        case "u": keyPressed("../Sounds/B3.mp3", "u"); break;
+    switch(kbsound){
+        case 'Piano':
+            switch (name) {
+                case "q": keyPressed("../Sounds/piano/C3.mp3", "q"); break;
+                case "w": keyPressed("../Sounds/piano/D3.mp3", "w"); break;
+                case "e": keyPressed("../Sounds/piano/E3.mp3", "e"); break;
+                case "r": keyPressed("../Sounds/piano/F3.mp3", "r"); break;
+                case "t": keyPressed("../Sounds/piano/G3.mp3", "t"); break;
+                case "y": keyPressed("../Sounds/piano/A3.mp3", "y"); break;
+                case "u": keyPressed("../Sounds/piano/B3.mp3", "u"); break;
+        
+                case "a": keyPressed("../Sounds/piano/C4.mp3", "a"); break;
+                case "s": keyPressed("../Sounds/piano/D4.mp3", "s"); break;
+                case "d": keyPressed("../Sounds/piano/E4.mp3", "d"); break;
+                case "f": keyPressed("../Sounds/piano/F4.mp3", "f"); break;
+                case "g": keyPressed("../Sounds/piano/G4.mp3", "g"); break;
+                case "h": keyPressed("../Sounds/piano/A4.mp3", "h"); break;
+                case "j": keyPressed("../Sounds/piano/B4.mp3", "j"); break;
+        
+                case "z": keyPressed("../Sounds/piano/C5.mp3", "z"); break;
+                case "x": keyPressed("../Sounds/piano/D5.mp3", "x"); break;
+                case "c": keyPressed("../Sounds/piano/E5.mp3", "c"); break;
+                case "v": keyPressed("../Sounds/piano/F5.mp3", "v"); break;
+                case "b": keyPressed("../Sounds/piano/G5.mp3", "b"); break;
+                case "n": keyPressed("../Sounds/piano/A5.mp3", "n"); break;
+                case "m": keyPressed("../Sounds/piano/B5.mp3", "m"); break;
+        
+        
+                // Black keys
+                case "1": keyPressed("../Sounds/piano/Db3.mp3", "1"); break;
+                case "2": keyPressed("../Sounds/piano/Eb3.mp3", "2"); break;
+                case "3": keyPressed("../Sounds/piano/Gb3.mp3", "3"); break;
+                case "4": keyPressed("../Sounds/piano/Ab3.mp3", "4"); break;
+                case "5": keyPressed("../Sounds/piano/Bb3.mp3", "5"); break;
+        
+                case "6": keyPressed("../Sounds/piano/Db4.mp3", "6"); break;
+                case "7": keyPressed("../Sounds/piano/Eb4.mp3", "7"); break;
+                case "8": keyPressed("../Sounds/piano/Gb4.mp3", "8"); break;
+                case "9": keyPressed("../Sounds/piano/Ab4.mp3", "9"); break;
+                case "0": keyPressed("../Sounds/piano/Bb4.mp3", "0"); break;
+        
+                case "-": keyPressed("../Sounds/piano/Db5.mp3", "-"); break;
+                case "=": keyPressed("../Sounds/piano/Eb5.mp3", "="); break;
+                case "Backspace": keyPressed("../Sounds/piano/Gb5.mp3", "backspace"); break;
+                case "]": keyPressed("../Sounds/piano/Ab5.mp3", "]"); break;
+                case "#": keyPressed("../Sounds/piano/Bb5.mp3", "#"); break;
+            }
+            break;
 
-        case "a": keyPressed("../Sounds/C4.mp3", "a"); break;
-        case "s": keyPressed("../Sounds/D4.mp3", "s"); break;
-        case "d": keyPressed("../Sounds/E4.mp3", "d"); break;
-        case "f": keyPressed("../Sounds/F4.mp3", "f"); break;
-        case "g": keyPressed("../Sounds/G4.mp3", "g"); break;
-        case "h": keyPressed("../Sounds/A4.mp3", "h"); break;
-        case "j": keyPressed("../Sounds/B4.mp3", "j"); break;
+            case 'Strings':
+                switch (name) {
+                    case "q": keyPressed("../Sounds/strings/C3s.mp3", "q"); break;
+                    case "w": keyPressed("../Sounds/strings/D3s.mp3", "w"); break;
+                    case "e": keyPressed("../Sounds/strings/E3s.mp3", "e"); break;
+                    case "r": keyPressed("../Sounds/strings/F3s.mp3", "r"); break;
+                    case "t": keyPressed("../Sounds/strings/G3s.mp3", "t"); break;
+                    case "y": keyPressed("../Sounds/strings/A3s.mp3", "y"); break;
+                    case "u": keyPressed("../Sounds/strings/B3s.mp3", "u"); break;
+            
+                    case "a": keyPressed("../Sounds/strings/C4s.mp3", "a"); break;
+                    case "s": keyPressed("../Sounds/strings/D4s.mp3", "s"); break;
+                    case "d": keyPressed("../Sounds/strings/E4s.mp3", "d"); break;
+                    case "f": keyPressed("../Sounds/strings/F4s.mp3", "f"); break;
+                    case "g": keyPressed("../Sounds/strings/G4s.mp3", "g"); break;
+                    case "h": keyPressed("../Sounds/strings/A4s.mp3", "h"); break;
+                    case "j": keyPressed("../Sounds/strings/B4s.mp3", "j"); break;
+            
+                    case "z": keyPressed("../Sounds/strings/C5s.mp3", "z"); break;
+                    case "x": keyPressed("../Sounds/strings/D5s.mp3", "x"); break;
+                    case "c": keyPressed("../Sounds/strings/E5s.mp3", "c"); break;
+                    case "v": keyPressed("../Sounds/strings/F5s.mp3", "v"); break;
+                    case "b": keyPressed("../Sounds/strings/G5s.mp3", "b"); break;
+                    case "n": keyPressed("../Sounds/strings/A5s.mp3", "n"); break;
+                    case "m": keyPressed("../Sounds/strings/B5s.mp3", "m"); break;
+            
+            
+                    // Black keys
+                    case "1": keyPressed("../Sounds/strings/Db3s.mp3", "1"); break;
+                    case "2": keyPressed("../Sounds/strings/Eb3s.mp3", "2"); break;
+                    case "3": keyPressed("../Sounds/strings/Gb3s.mp3", "3"); break;
+                    case "4": keyPressed("../Sounds/strings/Ab3s.mp3", "4"); break;
+                    case "5": keyPressed("../Sounds/strings/Bb3s.mp3", "5"); break;
+            
+                    case "6": keyPressed("../Sounds/strings/Db4s.mp3", "6"); break;
+                    case "7": keyPressed("../Sounds/strings/Eb4s.mp3", "7"); break;
+                    case "8": keyPressed("../Sounds/strings/Gb4s.mp3", "8"); break;
+                    case "9": keyPressed("../Sounds/strings/Ab4s.mp3", "9"); break;
+                    case "0": keyPressed("../Sounds/strings/Bb4s.mp3", "0"); break;
+            
+                    case "-": keyPressed("../Sounds/strings/Db5s.mp3", "-"); break;
+                    case "=": keyPressed("../Sounds/strings/Eb5s.mp3", "="); break;
+                    case "Backspace": keyPressed("../Sounds/strings/Gb5s.mp3", "backspace"); break;
+                    case "]": keyPressed("../Sounds/strings/Ab5s.mp3", "]"); break;
+                    case "#": keyPressed("../Sounds/strings/Bb5s.mp3", "#"); break;
+                }
+                break;
 
-        case "z": keyPressed("../Sounds/C5.mp3", "z"); break;
-        case "x": keyPressed("../Sounds/D5.mp3", "x"); break;
-        case "c": keyPressed("../Sounds/E5.mp3", "c"); break;
-        case "v": keyPressed("../Sounds/F5.mp3", "v"); break;
-        case "b": keyPressed("../Sounds/G5.mp3", "b"); break;
-        case "n": keyPressed("../Sounds/A5.mp3", "n"); break;
-        case "m": keyPressed("../Sounds/B5.mp3", "m"); break;
+                case 'Harp':
+            switch (name) {
+                case "q": keyPressed("../Sounds/harp/C3h.mp3", "q"); break;
+                case "w": keyPressed("../Sounds/harp/D3h.mp3", "w"); break;
+                case "e": keyPressed("../Sounds/harp/E3h.mp3", "e"); break;
+                case "r": keyPressed("../Sounds/harp/F3h.mp3", "r"); break;
+                case "t": keyPressed("../Sounds/harp/G3h.mp3", "t"); break;
+                case "y": keyPressed("../Sounds/harp/A3h.mp3", "y"); break;
+                case "u": keyPressed("../Sounds/harp/B3h.mp3", "u"); break;
+        
+                case "a": keyPressed("../Sounds/harp/C4h.mp3", "a"); break;
+                case "s": keyPressed("../Sounds/harp/D4h.mp3", "s"); break;
+                case "d": keyPressed("../Sounds/harp/E4h.mp3", "d"); break;
+                case "f": keyPressed("../Sounds/harp/F4h.mp3", "f"); break;
+                case "g": keyPressed("../Sounds/harp/G4h.mp3", "g"); break;
+                case "h": keyPressed("../Sounds/harp/A4h.mp3", "h"); break;
+                case "j": keyPressed("../Sounds/harp/B4h.mp3", "j"); break;
+        
+                case "z": keyPressed("../Sounds/harp/C5h.mp3", "z"); break;
+                case "x": keyPressed("../Sounds/harp/D5h.mp3", "x"); break;
+                case "c": keyPressed("../Sounds/harp/E5h.mp3", "c"); break;
+                case "v": keyPressed("../Sounds/harp/F5h.mp3", "v"); break;
+                case "b": keyPressed("../Sounds/harp/G5h.mp3", "b"); break;
+                case "n": keyPressed("../Sounds/harp/A5h.mp3", "n"); break;
+                case "m": keyPressed("../Sounds/harp/B5h.mp3", "m"); break;
+        
+        
+                // Black keys
+                case "1": keyPressed("../Sounds/harp/Db3h.mp3", "1"); break;
+                case "2": keyPressed("../Sounds/harp/Eb3h.mp3", "2"); break;
+                case "3": keyPressed("../Sounds/harp/Gb3h.mp3", "3"); break;
+                case "4": keyPressed("../Sounds/harp/Ab3h.mp3", "4"); break;
+                case "5": keyPressed("../Sounds/harp/Bb3h.mp3", "5"); break;
+        
+                case "6": keyPressed("../Sounds/harp/Db4h.mp3", "6"); break;
+                case "7": keyPressed("../Sounds/harp/Eb4h.mp3", "7"); break;
+                case "8": keyPressed("../Sounds/harp/Gb4h.mp3", "8"); break;
+                case "9": keyPressed("../Sounds/harp/Ab4h.mp3", "9"); break;
+                case "0": keyPressed("../Sounds/harp/Bb4h.mp3", "0"); break;
+        
+                case "-": keyPressed("../Sounds/harp/Db5h.mp3", "-"); break;
+                case "=": keyPressed("../Sounds/harp/Eb5h.mp3", "="); break;
+                case "Backspace": keyPressed("../Sounds/harp/Gb5h.mp3", "backspace"); break;
+                case "]": keyPressed("../Sounds/harp/Ab5h.mp3", "]"); break;
+                case "#": keyPressed("../Sounds/harp/Bb5h.mp3", "#"); break;
+            }
+            break;
 
+            case 'Glockenspiel':
+            switch (name) {
+                case "q": keyPressed("../Sounds/glockenspiel/C3g.mp3", "q"); break;
+                case "w": keyPressed("../Sounds/glockenspiel/D3g.mp3", "w"); break;
+                case "e": keyPressed("../Sounds/glockenspiel/E3g.mp3", "e"); break;
+                case "r": keyPressed("../Sounds/glockenspiel/F3g.mp3", "r"); break;
+                case "t": keyPressed("../Sounds/glockenspiel/G3g.mp3", "t"); break;
+                case "y": keyPressed("../Sounds/glockenspiel/A3g.mp3", "y"); break;
+                case "u": keyPressed("../Sounds/glockenspiel/B3g.mp3", "u"); break;
+        
+                case "a": keyPressed("../Sounds/glockenspiel/C4g.mp3", "a"); break;
+                case "s": keyPressed("../Sounds/glockenspiel/D4g.mp3", "s"); break;
+                case "d": keyPressed("../Sounds/glockenspiel/E4g.mp3", "d"); break;
+                case "f": keyPressed("../Sounds/glockenspiel/F4g.mp3", "f"); break;
+                case "g": keyPressed("../Sounds/glockenspiel/G4g.mp3", "g"); break;
+                case "h": keyPressed("../Sounds/glockenspiel/A4g.mp3", "h"); break;
+                case "j": keyPressed("../Sounds/glockenspiel/B4g.mp3", "j"); break;
+        
+                case "z": keyPressed("../Sounds/glockenspiel/C5g.mp3", "z"); break;
+                case "x": keyPressed("../Sounds/glockenspiel/D5g.mp3", "x"); break;
+                case "c": keyPressed("../Sounds/glockenspiel/E5g.mp3", "c"); break;
+                case "v": keyPressed("../Sounds/glockenspiel/F5g.mp3", "v"); break;
+                case "b": keyPressed("../Sounds/glockenspiel/G5g.mp3", "b"); break;
+                case "n": keyPressed("../Sounds/glockenspiel/A5g.mp3", "n"); break;
+                case "m": keyPressed("../Sounds/glockenspiel/B5g.mp3", "m"); break;
+        
+        
+                // Black keys
+                case "1": keyPressed("../Sounds/glockenspiel/Db3g.mp3", "1"); break;
+                case "2": keyPressed("../Sounds/glockenspiel/Eb3g.mp3", "2"); break;
+                case "3": keyPressed("../Sounds/glockenspiel/Gb3g.mp3", "3"); break;
+                case "4": keyPressed("../Sounds/glockenspiel/Ab3g.mp3", "4"); break;
+                case "5": keyPressed("../Sounds/glockenspiel/Bb3g.mp3", "5"); break;
+        
+                case "6": keyPressed("../Sounds/glockenspiel/Db4g.mp3", "6"); break;
+                case "7": keyPressed("../Sounds/glockenspiel/Eb4g.mp3", "7"); break;
+                case "8": keyPressed("../Sounds/glockenspiel/Gb4g.mp3", "8"); break;
+                case "9": keyPressed("../Sounds/glockenspiel/Ab4g.mp3", "9"); break;
+                case "0": keyPressed("../Sounds/glockenspiel/Bb4g.mp3", "0"); break;
+        
+                case "-": keyPressed("../Sounds/glockenspiel/Db5g.mp3", "-"); break;
+                case "=": keyPressed("../Sounds/glockenspiel/Eb5g.mp3", "="); break;
+                case "Backspace": keyPressed("../Sounds/glockenspiel/Gb5g.mp3", "backspace"); break;
+                case "]": keyPressed("../Sounds/glockenspiel/Ab5g.mp3", "]"); break;
+                case "#": keyPressed("../Sounds/glockenspiel/Bb5g.mp3", "#"); break;
+            }
+            break;
 
-        // Black keys
-        case "1": keyPressed("../Sounds/Db3.mp3", "1"); break;
-        case "2": keyPressed("../Sounds/Eb3.mp3", "2"); break;
-        case "3": keyPressed("../Sounds/Gb3.mp3", "3"); break;
-        case "4": keyPressed("../Sounds/Ab3.mp3", "4"); break;
-        case "5": keyPressed("../Sounds/Bb3.mp3", "5"); break;
-
-        case "6": keyPressed("../Sounds/Db4.mp3", "6"); break;
-        case "7": keyPressed("../Sounds/Eb4.mp3", "7"); break;
-        case "8": keyPressed("../Sounds/Gb4.mp3", "8"); break;
-        case "9": keyPressed("../Sounds/Ab4.mp3", "9"); break;
-        case "0": keyPressed("../Sounds/Bb4.mp3", "0"); break;
-
-        case "-": keyPressed("../Sounds/Db5.mp3", "-"); break;
-        case "=": keyPressed("../Sounds/Eb5.mp3", "="); break;
-        case "Backspace": keyPressed("../Sounds/Gb5.mp3", "backspace"); break;
-        case "]": keyPressed("../Sounds/Ab5.mp3", "]"); break;
-        case "#": keyPressed("../Sounds/Bb5.mp3", "#"); break;
+                default:
+                    console.log('get fucked');
     }
 
     console.log(pressedKeys) // Print list of pressed keys
@@ -315,7 +467,6 @@ function guideControl(type){
         default:
             break;
     }
-
     function updateDisplay(){
         guideStarted = true;
         keysToPress = guideKeys; // Set keysToPress array as list of guide keys -> show user what keys they need to press
@@ -327,6 +478,9 @@ function guideControl(type){
     }
 }
 
+function soundControl(noise){
+    kbsound = noise;
+}
 
 function getKeyFromBlackKey(name){
     let blackKey = "";
